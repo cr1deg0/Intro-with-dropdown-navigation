b45e5708ad050538d97f96cccfb3e5e0632e8baa
cr1deg0 <crisidego@gmail.com>
2022-06-21 21:42:14 +0100

    solution with keyboard navigation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Mobile main menu open/close functionality
 
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const navMenu = document.getElementById('primary-navigation');
+const navToggle = document.querySelector('button[aria-controls="primary-navigation"]');
+const navMenu = document.querySelector('nav[aria-label="Main"]');
 
 navToggle.addEventListener('click', controlNavMenu);
 
@@ -19,30 +19,99 @@ function controlNavMenu(e) {
     }
 }
 
-// Submenu open/close functionality, for mouse and keyboard users
+// Keyboard navigation and mouse hover event//
+// Select all navigation control items: buttons + links
+const navNodes = [... navMenu.querySelectorAll('button[aria-expanded][aria-controls], .nav-link, .nav-btn')];
 
-const submenuToggleList = document.querySelectorAll('.dropdown--title');
-const dropdownSubmenuList = document.querySelectorAll('.dropdown-submenu');
+let openMenuIndex = null;
 
-submenuToggleList.forEach((submenuToggle) => submenuToggle.addEventListener('click', controlSubMenu));
-dropdownSubmenuList.forEach((dropdownSubmenu) => dropdownSubmenu.addEventListener('focusout', closeSubMenu));
+// Add event listener to nav bar to close any open menus when focus is moved out
+document.querySelector('main').addEventListener('focusin', controlNavUnfocus);
 
-function controlSubMenu(e) {
-    const visibility = e.target.getAttribute('aria-expanded');
+navNodes.forEach((navNode) => {
+    // handle the navbar buttons with dropdown menus
+    if (navNode.hasAttribute('aria-controls')) {
+        
+        const dropdownMenu = navNode.parentNode.querySelector('ul');
+
+        // attach event listeners to each control button and submenu
+        navNode.addEventListener('click', controlButtonClick);
+        navNode.addEventListener('keydown', controlButtonKeyDown);
+        navNode.addEventListener('mouseover', controlButtonFocus);
+        dropdownMenu.addEventListener('keydown', controlDropdownKeyDown);    
+        dropdownMenu.addEventListener('mouseleave', controlDropdownFocusOut);
+        dropdownMenu.addEventListener('blur', controlDropdownFocusOut);
+        dropdownMenu.addEventListener('focusout', controlDropdownFocusOut);
+    } 
+});
+
+function toggleDropdownMenu (menu) {
+    // open/close dropdown menu by looking at the current visibility status
+
+    const visibility = menu.getAttribute('aria-expanded');
+
     if (visibility === 'false') {
-        e.target.setAttribute('aria-expanded', 'true');
+        menu.setAttribute('aria-expanded', 'true');
+        openMenuIndex = navNodes.indexOf(menu);
     }
     else {
-        e.target.setAttribute('aria-expanded', 'false');
+        menu.setAttribute('aria-expanded', 'false');
+        openMenuIndex = null;
     }
 }
 
-function closeSubMenu(e) {
+function controlButtonClick(e) {
 
-    const closeMenuBtn = e.target.parentNode.parentNode.parentNode.firstElementChild;
-    const lastItem = e.target.getAttribute('data-end');
+    const targetIndex = navNodes.indexOf(e.target);
+    
+    // if there is no menu already open, open the target menu
+    if (openMenuIndex === null) {
+        toggleDropdownMenu (e.target);
+    }
+    // if the target's menu is already open, close it 
+    else if (targetIndex === openMenuIndex) {
+        toggleDropdownMenu(e.target);
+    }
+    // in the menu open is not the target's, close it and open the target's menu
+    else {
+        toggleDropdownMenu(navNodes[openMenuIndex]);
+        toggleDropdownMenu(e.target);
+    }
+}
 
-    if (lastItem === "true") {
-        closeMenuBtn.setAttribute('aria-expanded', 'false');
+function controlButtonFocus(e) {
+    toggleDropdownMenu(e.target);
+}
+
+function controlDropdownFocusOut(e) {
+    toggleDropdownMenu(e.target.previousElementSibling);
+}
+
+function controlButtonKeyDown (e) {
+
+    const btnStatus = e.target.getAttribute('aria-expanded');
+ 
+    // if keyboard input is "Escape" and submenu open, close submenu and set focus in control button
+    if (btnStatus === 'true' && e.key === 'Escape') {
+        navNodes[openMenuIndex].focus();
+        toggleDropdownMenu(e.target);
+    }
+}
+
+function controlDropdownKeyDown(e) {
+
+    const parentNode = navNodes[openMenuIndex];
+    const btnStatus = parentNode.getAttribute('aria-expanded');
+    // close open menu on Escape
+    if (btnStatus === 'true' && e.key==='Escape') {
+        toggleDropdownMenu(parentNode);
+        parentNode.focus();
+    }    
+}
+
+function controlNavUnfocus (e) {
+    // if there is an open dropdown menu, close it
+    if (openMenuIndex !== null) {
+        toggleDropdownMenu(navNodes[openMenuIndex]);
     }
 }
